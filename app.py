a66a2bbb666f05ffb9e408e312cfb6ba2f42829d
siddhartharao17 <siddhartharao17@gmail.com>
2017-04-13 00:24:29 -0400

    added decoder in oauth.py

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ app.config['SECRET_KEY'] = 'top secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['OAUTH_CREDENTIALS'] = {
     'facebook': {
-        'id': '470154729788964',
-        'secret': '010cc08bd4f51e34f3f3e684fbdea8a7'
+        #'id': '470154729788964',
+        'id': '277308882695364',
+        'secret': '949643620832ced50226022166416435'
+        #'secret': '010cc08bd4f51e34f3f3e684fbdea8a7'
     },
     'twitter': {
         'id': '3RzWQclolxWZIMq5LJqzRZPTl',
